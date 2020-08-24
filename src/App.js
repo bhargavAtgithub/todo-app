1c46d47ae30b6749b18282455a479a21b7a8bb14
Bhargav <bhargav999reddy@gmail.com>
2020-08-24 23:53:10 +0530

    Todo Application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header/Header.component";
+import TodoList from "./components/TodoList/TodoList.component";
+import SubmitForm from "./components/SubmitForm/SubmitForm.component";
+
+class App extends React.Component {
+  state = {
+    tasks: [],
+  };
+
+  handleSubmit = (task) => {
+    this.setState({ tasks: [...this.state.tasks, task] });
+  };
+
+  handleDelete = (index) => {
+    const newArr = [...this.state.tasks];
+    newArr.splice(index, 1);
+    this.setState({ tasks: newArr });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="card frame">
+          <Header numTodos={this.state.tasks.length} />
+          <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} />
+          <SubmitForm onFormSubmit={this.handleSubmit} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
